121ac6de18ded1ec318db81b90761a4261b92e7e
vishal kumar singh <vishurajputmgr@gmail.com>
2022-01-05 17:11:18 +0530

    my first commit

---
+++ asset/main.js
@@ -0,0 +1,8 @@
+const menu = document.querySelectorAll('.menu')[0];
+const toggle = document.querySelectorAll('.toggle')[0];
+        
+
+        function toggleMenu() {
+            menu.classList.toggle('active');
+            toggle.classList.toggle('active');
+        };
